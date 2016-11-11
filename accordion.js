397dcd63ef65bf707c31fb584b1bbff3085294bd
Hernan Y.Ke <yuriqiao@gmail.com>
2016-11-11 08:01:21 -0500

    accordion step 2

--- accordion.js
+++ accordion.js
@@ -1,1 +1,13 @@
-accordion.js
+(function($){
+	$.fn.accordion = function(){
+		this.find("h3").addClass("accordion-header")
+		.next()
+		.addClass("accordion-panel")
+		.slideUp();
+
+		this.on("click",".accordion-header",function(e){
+			var next = $(e.target).next();
+			next.slideToggle();
+		})
+	};
+})(jQuery)
