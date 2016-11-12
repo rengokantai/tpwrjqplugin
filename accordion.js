69d04d2387f9a64cd846a63f8cc178e34ba49081
Hernan Y.Ke <yuriqiao@gmail.com>
2016-11-12 10:26:48 -0500

    toggle and change other's state

--- accordion.js
+++ accordion.js
@@ -1,13 +1,30 @@
 (function($){
-	$.fn.accordion = function(){
+	$.fn.accordion = function(options){
+
+		var settings = $.extend({
+
+		},$.fn.accordion.defaults, options)
 		this.find("h3").addClass("accordion-header")
 		.next()
 		.addClass("accordion-panel")
 		.slideUp();
+		var $this = this;
 
 		this.on("click",".accordion-header",function(e){
 			var next = $(e.target).next();
-			next.slideToggle();
+			var isActive = next.attr("data-active");
+
+
+			if(settings.distinct){
+				$this.find("[data-active]").removeAttr("data-active").slideToggle();
+			}
+			next.attr("data-active",true).slideToggle();
 		})
+		return this;
 	};
-})(jQuery)
+
+
+	$.fn.accordion.defaults = {
+		distinct:false
+	};
+})(jQuery);
